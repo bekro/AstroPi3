c3533b4ebcebdb360560d4349825f772d721cff4
Benjamin Krosse <benjaminkrosse@Benjamins-iMac.fritz.box>
2019-05-30 11:36:28 +0200

    Remove filecheck

--- install_PoleMaster.sh
+++ install_PoleMaster.sh
@@ -24,13 +24,13 @@ cd ~
 wget https://www.qhyccd.com/uploadfile/2018/1222/20181222054634222.zip
 unzip 20181222054634222.zip -d .
 
-if [ -e PoleMaster_Qt-for-RPI-Ubuntu-1.3.5.0.deb ]
-then
-    echo "Great success!"
-else
-    echo "Package not found. Bailing out."
-    exit
-fi
+# if [ -e PoleMaster_Qt-for_RPI-Ubuntu-1.3.5.0.deb ]
+# then
+#     echo "Great success!"
+# else
+#     echo "Package not found. Bailing out."
+#     exit
+# fi
 
 display "Installing the PoleMaster_Qt-for-RPI-Ubuntu-1 package"
 
